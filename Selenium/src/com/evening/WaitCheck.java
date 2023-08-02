00262ff9bc6405b96c0bc1c6d08bea492775d6be
sibithemaster <128968033+sibithemaster@users.noreply.github.com>
2023-08-02 13:49:11 +0530

    new update with fluent wait

--- Selenium/src/com/evening/WaitCheck.java
+++ Selenium/src/com/evening/WaitCheck.java
@@ -29,7 +29,19 @@ public class WaitCheck {
 		//element not visible exception
 		WebDriverWait wait = new WebDriverWait(driver,30);
 		wait.until(ExpectedConditions.visibilityOf(firstName)).sendKeys("sibi");
-		
+
+		//Declare and initialise a fluent wait
+                FluentWait wait = new FluentWait(driver);
+               //Specify the timout of the wait
+               wait.withTimeout(5000, TimeUnit.MILLISECONDS);
+               //Sepcify polling time
+               wait.pollingEvery(250, TimeUnit.MILLISECONDS);
+              //Specify what exceptions to ignore
+              wait.ignoring(NoSuchElementException.class)
+
+             //This is how we specify the condition to wait on.
+            //This is what we will explore more in this chapter
+            wait.until(ExpectedConditions.alertIsPresent());
 	}
 
 }
